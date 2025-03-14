78359e84b8f4df549243dd6ff902618ce9782570
Landon Yurica <ayurica@gmail.com>
2025-03-14 12:51:04 -0700

    Initial Commit

---
+++ GroceryApp/GroceryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryAppApp.swift
+//  GroceryApp
+//
+//  Created by Landon Yurica on 3/14/25.
+//
+
+import SwiftUI
+
+@main
+struct GroceryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
